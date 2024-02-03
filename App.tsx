97b0776ef5764fac28312b83d9a1763587db395e
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-02-03 17:00:45 +0500

    App Icon

--- App.tsx
+++ App.tsx
@@ -2,13 +2,19 @@ import {
   // Text,
   View,
   StyleSheet,
+  // Platform,
   // useColorScheme,
   // ScrollViewComponent,
 } from 'react-native';
-import React from 'react';
+import React from 'react'; // , { useEffect }
 import SplashScreen from './navigation/screens/SplashScreen';
 
 const App = (): JSX.Element => {
+  // useEffect(() => {
+  //   if (Platform.OS === 'android') {
+  //     SplashScreen.hide();
+  //   }
+  // }, []);
   return (
     <View style={styles.container}>
       {/* <Text style={styles.title}>
